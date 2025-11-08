3faa3dbbfcfca6bf28a22ecefd41ae9a904c396c
Bhavika Agarwal <bhavi@DESKTOP-6U22E9L>
2025-11-08 13:11:23 -0500

    llq

--- LLQ.hpp
+++ LLQ.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Interfaces.hpp"
 #include "LinkedList.hpp"
 #include <stdlib.h>
@@ -11,18 +10,44 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLQ();
+    LLQ() : list() {}
 
     // Insertion
-    void enqueue(const T& item) override;
+    void enqueue(const T& item) override
+    {
+        list.addTail();
+    }
 
     // Deletion
-    T dequeue() override;
+    T dequeue() override
+    {
+        if(list.getCount() == 0)
+        {
+            throw std::runtime_error("Linked-List Queue is empty.");
+        }
+
+        T returnValue = list.getHead()->data;
+        list.removeHead();
+
+        return returnValue;
+
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override
+    {
+        if(list.getCount() == 0)
+        {
+            throw std::runtime_error("Linked-List Queue is empty.");
+        }
+
+        return list.getHead()->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override
+    {
+        return list.getCount();
+    }
 
 };
